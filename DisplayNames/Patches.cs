d91b683ec487c2941b4cf243fd8791df8693adb3
Luna <lennon.m.craig@gmail.com>
2023-06-23 18:52:54 -0400

    revert to patched string builder for this branch
    patching scoreboard string builder appears to be more stable as of now, also closer to the base game appearance than enabling the lines
    maybe more rigid too since this is like actually used lmao im rambling.
    kinda mentally drained atm

--- DisplayNames/Patches.cs
+++ DisplayNames/Patches.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 using ScoreboardAttributes;
 using Photon.Pun;
 using GorillaNetworking;
+using UnityEngine.UI;
 
 namespace DisplayNames
 {
     [HarmonyPatch]
     internal class Patches
     {
+        public static GorillaScoreBoard scoreboardInstance;
         [HarmonyPatch(typeof(GorillaLocomotion.Player), "Awake"), HarmonyPostfix]
         private static void GameInitialized()
         {
@@ -16,37 +18,70 @@ namespace DisplayNames
             Photon.Pun.PhotonNetwork.LocalPlayer.SetCustomProperties(new ExitGames.Client.Photon.Hashtable() { { Main.Instance.ChannelId, Main.Instance.CustomName } });
         }
 
-        [HarmonyPatch(typeof(GorillaScoreBoard), "RedrawPlayerLines"), HarmonyPostfix]
-        private static void RedrawLines(GorillaScoreBoard __instance)
-            => RedrawLinesInternal(__instance);
-
-        [HarmonyPatch(typeof(GorillaScoreBoard), "InfrequentUpdate"), HarmonyPostfix]
-        private static void InfreqUpdate(GorillaScoreBoard __instance)
-            => RedrawLinesInternal(__instance);
-
-        internal static void RedrawLinesInternal(GorillaScoreBoard __instance)
+        // experimental support for using actual scoreboard lines rather than
+        // a replacement string builder lives on the playerlines branch.
+        [HarmonyPatch(typeof(GorillaScoreBoard), "RedrawPlayerLines"), HarmonyPrefix]
+        private static bool RedrawLines(GorillaScoreBoard __instance)
         {
+            scoreboardInstance = __instance;
+            __instance.lines.Sort((GorillaPlayerScoreboardLine line1, GorillaPlayerScoreboardLine line2) => line1.playerActorNumber.CompareTo(line2.playerActorNumber));
             __instance.boardText.text = __instance.GetBeginningString();
-            foreach (GorillaPlayerScoreboardLine line in __instance.lines)
+            __instance.buttonText.text = "";
+            for (int i = 0; i < __instance.lines.Count; i++)
             {
-                if (!line.playerName.gameObject.activeSelf)
-                    line.playerName.gameObject.SetActive(true);
+                try
+                {
+                    __instance.lines[i].gameObject.GetComponent<RectTransform>().localPosition = new Vector3(0f, (float)(__instance.startingYValue - __instance.lineHeight * i), 0f);
+                    if (__instance.lines[i].linePlayer != null)
+                    {
+                        Text text = __instance.boardText;
+                        if (__instance.lines[i].linePlayer.CustomProperties.ContainsKey(Main.Instance.ChannelId))
+                        {
+                            if (GorillaComputer.instance.CheckAutoBanListForName(TruncateString((string)__instance.lines[i].linePlayer.CustomProperties[Main.Instance.ChannelId])))
+                            {
+                                text.text = text.text + "\n " + TruncateString((string)__instance.lines[i].linePlayer.CustomProperties[Main.Instance.ChannelId]);
+                            }
+                            else { text.text = text.text + "\n " + "Bitch"; }
+                            PlayerTexts.RegisterAttribute(__instance.lines[i].linePlayer.NickName, __instance.lines[i].linePlayer);
+                        }
+                        else
+                        {
+                            text.text = text.text + "\n " + __instance.NormalizeName(true, __instance.lines[i].linePlayer.NickName);
+                        }
 
-                if (!line.linePlayer.CustomProperties.TryGetValue(Main.Instance.ChannelId, out object value) && !line.linePlayer.IsLocal)
-                    continue;
-
-                string playerText = !line.linePlayer.IsLocal ? (string)value : Main.Instance.CustomName;
-
-                line.playerName.text = TruncateString(playerText);
-                PlayerTexts.RegisterAttribute(line.linePlayer.NickName, line.linePlayer);
+                        if (__instance.lines[i].linePlayer != PhotonNetwork.LocalPlayer)
+                        {
+                            if (__instance.lines[i].reportButton.isActiveAndEnabled)
+                            {
+                                Text text2 = __instance.buttonText;
+                                text2.text += "MUTE                                REPORT\n";
+                            }
+                            else
+                            {
+                                Text text3 = __instance.buttonText;
+                                text3.text += "MUTE                HATE SPEECH    TOXICITY      CHEATING      CANCEL\n";
+                            }
+                        }
+                        else
+                        {
+                            Text text4 = __instance.buttonText;
+                            text4.text += "\n";
+                        }
+                    }
+                }
+                catch
+                {
+                }
             }
+            return false;
         }
 
         [HarmonyPatch(typeof(VRRig), "InitializeNoobMaterialLocal"), HarmonyPostfix]
         private static void VRRigPatch(VRRig __instance)
         {
-            if (!__instance.isOfflineVRRig) 
-            { 
+
+            if (!__instance.isOfflineVRRig)
+            {
                 PhotonView VRRigPhotonView = (PhotonView)AccessTools.Field(__instance.GetType(), "photonView").GetValue(__instance);
                 if (!VRRigPhotonView.Owner.CustomProperties.TryGetValue(Main.Instance.ChannelId, out object value))
                     return;
@@ -57,13 +92,14 @@ namespace DisplayNames
                 __instance.playerText.text = TruncateString(Main.Instance.CustomName);
             }
         }
-        
+
         private static string TruncateString(string str)
         {
             if (str.Length > Main.MaxCharacters)
             {
                 return str.Substring(0, Main.MaxCharacters);
-            } else return str;
+            }
+            else return str;
         }
     }
 }
